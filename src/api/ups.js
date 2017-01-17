aad0a8f026de9c107e049423cdd05f78eea300dc
Andrew Koltyakov <andrew.koltyakov@arvosys.com>
2017-01-17 17:22:32 +0300

    UPS basic methods

--- src/api/ups.js
+++ src/api/ups.js
@@ -4,6 +4,65 @@ var spf = spf || {};
 
 spf.UPS = function(request) {
 
+    /* SOAP */
+
+    this.getUserProfileByName = function(data) {
+        var headers = {};
+        var soapBody = '';
+        var soapTemplate = Handlebars.compile(
+            '<?xml version="1.0" encoding="utf-8"?>' +
+            '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+                '<soap:Body>' +
+                    '<GetUserProfileByName xmlns="http://microsoft.com/webservices/SharePointPortalServer/UserProfileService">' +
+                        '<AccountName>{{ accountName }}</AccountName>' +
+                    '</GetUserProfileByName>' +
+                '</soap:Body>' +
+            '</soap:Envelope>'
+        );
+
+        soapBody = soapTemplate(data);
+
+        headers["Accept"] = "application/xml, text/xml, */*; q=0.01";
+        headers["Content-Type"] = "text/xml;charset=\"UTF-8\"";
+        headers["X-Requested-With"] = "XMLHttpRequest";
+        headers["Content-Length"] = soapBody.length;
+
+        return request.post(data.baseUrl + '/_vti_bin/UserProfileService.asmx', {
+            headers: headers,
+            body: soapBody,
+            json: false
+        });
+    };
+
+    this.getUserPropertyByAccountName = function(data) {
+        var headers = {};
+        var soapBody = '';
+        var soapTemplate = Handlebars.compile(
+            '<?xml version="1.0" encoding="utf-8"?>' +
+            '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+                '<soap:Body>' +
+                    '<GetUserPropertyByAccountName xmlns="http://microsoft.com/webservices/SharePointPortalServer/UserProfileService">' +
+                        '<accountName>{{ accountName }}</accountName>' +
+                        '<propertyName>{{ propertyName }}</propertyName>' +
+                    '</GetUserPropertyByAccountName>' +
+                '</soap:Body>' +
+            '</soap:Envelope>'
+        );
+
+        soapBody = soapTemplate(data);
+
+        headers["Accept"] = "application/xml, text/xml, */*; q=0.01";
+        headers["Content-Type"] = "text/xml;charset=\"UTF-8\"";
+        headers["X-Requested-With"] = "XMLHttpRequest";
+        headers["Content-Length"] = soapBody.length;
+
+        return request.post(data.baseUrl + '/_vti_bin/UserProfileService.asmx', {
+            headers: headers,
+            body: soapBody,
+            json: false
+        });
+    };
+
     this.modifyUserPropertyByAccountName = function(data) {
         var headers = {};
         var soapBody = '';
@@ -51,7 +110,7 @@ spf.UPS = function(request) {
             return data;
         });
 
-        soapBody = soapTemplate(data); // .replace(/\\"/g, '"').replace('"<', '<').replace('>"', '>');
+        soapBody = soapTemplate(data);
 
         headers["SOAPAction"] = "http://microsoft.com/webservices/SharePointPortalServer/UserProfileService/ModifyUserPropertyByAccountName";
         headers["Accept"] = "application/xml, text/xml, */*; q=0.01";
@@ -64,6 +123,23 @@ spf.UPS = function(request) {
             body: soapBody,
             json: false
         });
+    };
+
+    /* REST */
+
+    this.getPropertiesFor = function(data) {
+        var methodUrl = `${data.baseUrl}/_api/sp.userprofiles.peoplemanager` +
+            `/getpropertiesfor(` +
+                `accountName='${encodeURIComponent(data.accountName)}')`;
+        return request.get(methodUrl);
+    };
+
+    this.getUserProfilePropertyFor = function(data) {
+        var methodUrl = `${data.baseUrl}/_api/sp.userprofiles.peoplemanager` +
+            `/getuserprofilepropertyfor(` +
+                `accountName='${encodeURIComponent(data.accountName)}',` +
+                `propertyname='${data.propertyName}')`;
+        return request.get(methodUrl);
     };
 
     return this;
--- test/ups-test.js
+++ test/ups-test.js
@@ -1,42 +1,133 @@
 var path = require('path');
 var cpass = new (require('cpass'));
+var parseString = require('xml2js').parseString;
 
 var Screwdriver = require(__dirname + "/../src/index");
 
-var configPath = path.join(__dirname + "/../config/_private.conf.json");
-var context = require(configPath);
+var configPath = path.join(__dirname + "/config/_private.conf.json");
+var config = require(configPath);
+var context = config.context;
 if (context.password) {
     context.password = cpass.decode(context.password);
 }
 
 var screw = new Screwdriver(context);
 
-// ===========================================
+// ================================
 
-var data = {
-    baseUrl: context.siteUrl,
-    accountName: 'i:0#.f|membership|testprofile@contoso.com',
-    newData: [{
-        isPrivacyChanged: false,
-        isValueChanged: true,
-        privacy: 'NotSet',
-        name: 'SPS-Birthday',
-        values: [ '10.03' ]
-    }, {
-        isPrivacyChanged: false,
-        isValueChanged: true,
-        privacy: 'NotSet',
-        name: 'SPS-Department',
-        values: [ 'Administration' ]
-    }]
+var getUserProfileByName = function() {
+
+    var data = {
+        baseUrl: context.siteUrl,
+        accountName: config.data.accountName
+    };
+
+    screw.ups.getUserProfileByName(data)
+        .then(function(response) {
+            parseString(response.body, function(err, result) {
+                console.log('Response:', result['soap:Envelope']['soap:Body'][0]['GetUserProfileByNameResponse'][0]['GetUserProfileByNameResult'][0]);
+            });
+        })
+        .catch(function(err) {
+            console.log('Error:', err.message);
+        });
+
 };
+// getUserProfileByName();
 
-screw.ups.modifyUserPropertyByAccountName(data)
-    .then(function(response) {
-        console.log('Response:', response.body);
-    })
-    .catch(function(err) {
-        console.log('Error:', err.message);
-    });
+// ================================
 
-// ===========================================
+var modifyUserPropertyByAccountName = function() {
+
+    var data = {
+        baseUrl: context.siteUrl,
+        accountName: config.data.accountName,
+        newData: [{
+            isPrivacyChanged: false,
+            isValueChanged: true,
+            privacy: 'NotSet',
+            name: 'SPS-Birthday',
+            values: [ '10.03' ]
+        }, {
+            isPrivacyChanged: false,
+            isValueChanged: true,
+            privacy: 'NotSet',
+            name: 'SPS-Department',
+            values: [ 'Administration' ]
+        }]
+    };
+
+    screw.ups.modifyUserPropertyByAccountName(data)
+        .then(function(response) {
+            console.log('Response:', response.body);
+        })
+        .catch(function(err) {
+            console.log('Error:', err.message);
+        });
+
+};
+// modifyUserPropertyByAccountName();
+
+// ================================
+
+var getUserPropertyByAccountName = function() {
+
+    var data = {
+        baseUrl: context.siteUrl,
+        accountName: config.data.accountName,
+        propertyName: 'SPS-Birthday'
+    };
+
+    screw.ups.getUserPropertyByAccountName(data)
+        .then(function(response) {
+            parseString(response.body, function(err, result) {
+                console.log('Response:', result['soap:Envelope']['soap:Body'][0]['GetUserPropertyByAccountNameResponse'][0]['GetUserPropertyByAccountNameResult'][0]);
+            });
+        })
+        .catch(function(err) {
+            console.log('Error:', err.message);
+        });
+
+};
+// getUserPropertyByAccountName();
+
+// ================================
+
+var getUserProfilePropertyFor = function() {
+
+    var data = {
+        baseUrl: context.siteUrl,
+        accountName: config.data.accountName,
+        propertyName: 'SPS-Birthday'
+    };
+
+    screw.ups.getUserProfilePropertyFor(data)
+        .then(function(response) {
+            console.log('Response:', response.body);
+        })
+        .catch(function(err) {
+            console.log('Error:', err.message);
+        });
+
+};
+// getUserProfilePropertyFor();
+
+// ================================
+
+var getPropertiesFor = function() {
+
+    var data = {
+        baseUrl: context.siteUrl,
+        accountName: config.data.accountName
+    };
+
+    screw.ups.getPropertiesFor(data)
+        .then(function(response) {
+            console.log('Response:', response.body);
+        })
+        .catch(function(err) {
+            console.log('Error:', err.message);
+        });
+
+};
+// getPropertiesFor();
