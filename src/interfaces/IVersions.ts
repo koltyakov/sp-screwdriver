f3ec66a0965b0112839d59addf07cd05954397f7
Andrew Koltyakov <koltyakov@gmail.com>
2017-08-12 15:40:34 +0300

    Versions API implementation in TS

---
+++ src/interfaces/IVersions.ts
@@ -0,0 +1,15 @@
+export interface IDocumentVersions {
+    baseUrl?: string;
+    fileName: string;
+}
+
+export interface IDocumentVersion extends IDocumentVersions {
+    fileVersion: string;
+}
+
+export interface IItemVersions {
+    baseUrl?: string;
+    listId: string;
+    itemId: number;
+    fieldName: string;
+}
